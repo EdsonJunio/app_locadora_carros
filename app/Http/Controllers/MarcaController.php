f89b29b8cced729322e3b2cfe2ac5b509c831453
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2024-01-03 11:21:23 -0300

    Feat: Validacao parte 3 validando parametros e a importancia do Accept

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -28,6 +28,18 @@ class MarcaController extends Controller
 
     public function store(Request $request)
     {
+        $regras = [
+            'nome' => 'required|unique:marcas',
+            'imagem' => 'required'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute e obrigatorio',
+            'nome.unique' => 'O nome da marca ja existe'
+        ];
+
+        $request->validate($regras, $feedback);
+
         $marca = $this->marca->create($request->all());
         return response()->json($marca, 201);
     }
